b8834efff128a6b7751ab512580021efffae388e
Dhruv Jani <Dhruvjani4321@gmail.com>
2023-11-20 11:53:19 +0530

    push on monday

--- spring-boot-java-app/src/main/java/com/wecp/SpringBootApp.java
+++ spring-boot-java-app/src/main/java/com/wecp/SpringBootApp.java
@@ -19,4 +19,4 @@ public class SpringBootApp implements CommandLineRunner{
 		
 		
 	}
-} 
+}  
--- spring-boot-java-app/src/main/java/com/wecp/entities/PersonTransaction.java
+++ spring-boot-java-app/src/main/java/com/wecp/entities/PersonTransaction.java
@@ -25,6 +25,14 @@ public class PersonTransaction {
 	 @OneToMany(mappedBy = "personTransaction")
 	    private List<Product> products;
 
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
 	public Long getId() {
 		return id;
 	}
